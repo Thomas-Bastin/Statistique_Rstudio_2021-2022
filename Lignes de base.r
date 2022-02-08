60173048418dd063d2feca9e635a18e76b937579
Thomas-Bastin <thomas.bastin@student.hepl.be>
2022-02-08 17:58:35 +0100

    :sparkles: Add Step 01, and some usefull code

---
+++ Lignes de base.r
@@ -0,0 +1,4 @@
+rm(list=ls()) # vider la memoire
+printf <- function(...) cat (sprintf(...)) # creer une fonction printf  
+cls <- function() printf("\014")           # creer une fonction pour effacer la console
+cls()
---
+++ WD-Etape I/I Statistique Descriptive à une Dimension.R
@@ -0,0 +1,20 @@
+################################################################################
+#
+#   Arnone Matteo
+#   Bastin Thomas
+#   2201
+#   I. Statistique Descriptive à une Dimension
+#
+################################################################################
+
+#HEADER
+# Vider la memoire
+rm(list=ls()) 
+# creer une fonction printf  
+printf <- function(...) cat (sprintf(...)) 
+
+# creer une fonction pour effacer la console
+cls <- function() printf("\014")          
+cls()
+#FIN HEADER
+
