4c4d780b6211027217198c6372ec1b79ef6e5447
Thomas-Bastin <thomas.bastin@student.hepl.be>
2022-02-14 17:02:43 +0100

    :construction: Svd02

--- I Statistique Descriptive à une Dimension/1. Recuperation et mise en forme des données.R
+++ I Statistique Descriptive à une Dimension/1. Recuperation et mise en forme des données.R
@@ -3,7 +3,7 @@
 #   Arnone Matteo                                                             #
 #   Bastin Thomas                                                             #
 #   2201                                                                      #
-#   I.1. Recuperation et mise en forme des donn?es                            #
+#   I.1. Recuperation et mise en forme des données                            #
 #                                                                             #
 ###############################################################################
 
@@ -16,13 +16,34 @@ printf <- function(...) cat (sprintf(...))
 # creer une fonction pour effacer la console
 cls <- function() printf("\014")          
 cls()
-
+# Set WorkingDirectory
 setwd("E:\\HEPL\\R\\Statistique_Rstudio_2021-2022\\I Statistique Descriptive à une Dimension")
 #FIN HEADER
 
-grossistes <- data.frame()
+
+#1. Importation de valeur dans grossistes
 grossistes <- read.csv(file = "WholesaleCustomersData.csv", sep = ",", na.strings = "NA", header = TRUE)
 summary(grossistes)
+str(grossistes)
 
+#Le summary montre que l'on a importé certaines valeurs qualitative en temps que quantitative (Channel, Region)
 
+#le str(grossistes) permet de voir les types des collonnes, ainsi que les premières variables de chaque colonnes
+#Ainsi que les 8 colonnes(Variables) et les 439 Lignes (obs).
+
+#2. Il y a 8 colonnes(variables) et 439 Lignes(enregistrement) 
+
+#On met en variable stat qualitative(factor) la colonnes Channel et Region
+grossistes$Channel <- as.factor(grossistes$Channel)
+grossistes$Region <- as.factor(grossistes$Region)
 summary(grossistes)
+str(grossistes)
+
+
+#3. Recuperation Nom Ligne, et Nom Colonnes:
+
+
+#4. Créer une variable produitsFrais contenant la 3ème colonne du dataset de 3 manières différentes. 
+produitsFrais1 <- grossistes$Fresh #En Colonnes
+produitsFrais2 <- grossistes[3]    #En DataSet
+
